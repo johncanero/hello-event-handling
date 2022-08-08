f1464de5e39fb1f737eca39d99aa8d46420a0d87
John Canero <johncanero.official@gmail.com>
2022-08-08 10:31:43 +0800

    Apply Starting Files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+        <h1>Hello</h1>
+        <input type="text" placeholder="What's your name?" />
+        <button>Submit</button>
+   </div>
   );
 }
 
